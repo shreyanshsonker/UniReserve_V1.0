5081944bf7b42b97df6ae037a58fb520c70622a8
Shreyansh Sonker <shreyanshsonkar95@gmail.com>
2026-04-03 18:22:03 +0530

    old codebase removed

--- unireserve-backend/apps/facilities/serializers/rule.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from apps.facilities.models import BookingRule
-
-class BookingRuleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BookingRule
-        fields = '__all__'
-        read_only_fields = ('id', 'updated_at', 'updated_by')
--- unireserve-backend/apps/notifications/serializers/notification.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from apps.notifications.models import Notification
-
-class NotificationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notification
-        fields = '__all__'
-        read_only_fields = ('id', 'user', 'notification_type', 'title', 'body', 'related_booking', 'sent_via_email', 'email_sent_at', 'created_at')
